74dd0a036858cc71b26df64512e8ffbd62ffc492
monson2002 <monsonreji2002@gmail.com>
2023-11-20 22:08:56 +0530

    Fixed all bugs 10:08pm 20-11-23

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo','module:metro-react-native-babel-preset'],
+    plugins: ["nativewind/babel",["module:react-native-dotenv", {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env",
+        "safe": false,
+        "allowUndefined": true,
+        "verbose": false
+      }]]
   };
 };
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   plugins: [
+//     ["module:react-native-dotenv", {
+//       "envName": "APP_ENV",
+//       "moduleName": "@env",
+//       "path": ".env",
+//       "safe": false,
+//       "allowUndefined": true,
+//       "verbose": false
+//     }]
+//   ]
+// };
